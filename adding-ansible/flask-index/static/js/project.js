ecacab0f7a15e8558bbd8f87b239e136208733bc
Selifka <sharayahcorcoran@gmail.com>
2018-02-22 23:53:41 -0500

    Adding some JS and modifying CSS

--- adding-ansible/flask-index/static/js/project.js
+++ adding-ansible/flask-index/static/js/project.js
@@ -21,3 +21,27 @@ $(document).ready(function(){
     }  // End if
   });
 });
+
+// skills chart
+  $(document).ready(function(e) {
+  //var windowBottom = $(window).height();
+  var index=0;
+  $(document).scroll(function(){
+    var top = $('#skills').height()-$(window).scrollTop();
+    console.log(top)
+    if(top<-300){
+      if(index==0){ 
+      
+        $('.chart').easyPieChart({
+          easing: 'easeOutBounce',
+          onStep: function(from, to, percent) {
+            $(this.el).find('.percent').text(Math.round(percent));
+          }
+        });
+      
+        }
+      index++;
+    }
+  })
+  //console.log(nagativeValue)
+  });
