bc6516a0aff3ce0f9b95ad5abc2605ea3cfaac6d
Selifka <sba37@wildcats.unh.edu>
2018-02-23 14:48:32 -0500

    Making a few more chcanges to style

--- adding-ansible/flask-index/static/js/project.js
+++ adding-ansible/flask-index/static/js/project.js
@@ -23,25 +23,26 @@ $(document).ready(function(){
 });
 
 // skills chart
-  $(document).ready(function(e) {
-  //var windowBottom = $(window).height();
-  var index=0;
-  $(document).scroll(function(){
-    var top = $('#skills').height()-$(window).scrollTop();
-    console.log(top)
-    if(top<-300){
-      if(index==0){ 
-      
-        $('.chart').easyPieChart({
-          easing: 'easeOutBounce',
-          onStep: function(from, to, percent) {
-            $(this.el).find('.percent').text(Math.round(percent));
-          }
-        });
-      
+$(document).ready(function(e) {
+//var windowBottom = $(window).height();
+var index=0;
+
+$(document).scroll(function(){
+
+  var top = $('#skills').height()-$(window).scrollTop();
+
+  if(top<-1000){
+    if(index==0){ 
+    
+      $('.chart').easyPieChart({
+        easing: 'easeOutBounce',
+        onStep: function(from, to, percent) {
+          $(this.el).find('.percent').text(Math.round(percent));
         }
-      index++;
-    }
-  })
-  //console.log(nagativeValue)
-  });
+      });
+    
+      }
+    index++;
+  }
+})
+});
