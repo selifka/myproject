b81ba4c8d4d8da9194a0b9ba99b7f6e501bcba2a
Selifka <sba37@wildcats.unh.edu>
2018-03-13 16:54:17 -0400

    Update commit for small HTML changes

--- static/js/project.js
+++ static/js/project.js
@@ -21,28 +21,3 @@ $(document).ready(function(){
     }  // End if
   });
 });
-
-// skills chart
-$(document).ready(function(e) {
-//var windowBottom = $(window).height();
-var index=0;
-
-$(document).scroll(function(){
-
-  var top = $('#skills').height()-$(window).scrollTop();
-
-  if(top<-1000){
-    if(index==0){ 
-    
-      $('.chart').easyPieChart({
-        easing: 'easeOutBounce',
-        onStep: function(from, to, percent) {
-          $(this.el).find('.percent').text(Math.round(percent));
-        }
-      });
-    
-      }
-    index++;
-  }
-})
-});
