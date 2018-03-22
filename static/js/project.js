4a936af4ab309c5a012c1b86a6517149dc040676
Selifka <sba37@wildcats.unh.edu>
2018-03-22 14:51:54 -0400

    Adding JS, cleaning up files, finalized stylistic changes

--- static/js/project.js
+++ static/js/project.js
@@ -1,45 +1,142 @@
-$(document).ready(function(){
-  // Add scrollspy to <body>
-  $('body').scrollspy({target: ".navbar", offset: 60});   
+/***
+Project Javascript for smooth scrolling, auto closing navbar, and animation
+***/
 
-  // Add smooth scrolling on all links inside the navbar
-  $("#navbarNav a").on('click', function(event) {
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+// Shows the active div your on with scroll
+$('body').scrollspy({
+  target: "#navbarNav", offset: 60
+});   
 
-      // Store hash
-      var hash = this.hash;
+// Smooth scrolling for js-scroll-trigger class
+$(".js-scroll-trigger[href^='#']").on('click', function(e) {
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({scrollTop: $(hash).offset().top}, 800, function(){
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    }  // End if
+  //prevent the default click behavior 
+  e.preventDefault();
+
+  //store this has for use
+  var hash = this.hash;
+
+  //animate 
+  $('html, body').animate({
+      scrollTop: $(hash).offset().top 
+    }, 800, function(){
+
+      //when done add hash to url
+      window.location.hash = hash;
   });
 });
 
-$('.navbar-collapse').click(function(){
-    $(".navbar-collapse").collapse('hide');
+// Closes the menu automatically when link is clicked.
+$('.js-scroll-trigger').click(function() {
+  $('.navbar-collapse').collapse('hide');
 });
 
-$("#banner a").on('click', function(event) {
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+//Revealing images and text on scroll/load 
+window.sr = ScrollReveal();
+  
+  //Navbar
+  sr.reveal('.navbar', {
+    duration: 2000,
+    origin: 'bottom'
+  });
 
-      // Store hash
-      var hash = this.hash;
+  //Banner section
+  sr.reveal('.banner-greeting', {
+    duration: 2000,
+    origin: 'top'
+  });
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({scrollTop: $(hash).offset().top}, 800, function(){
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    }  // End if
+  sr.reveal('.banner-btn-js', {
+    duration: 2000,
+    delay: 600,
+    origin: 'bottom'
+  });
+    
+  //About section
+  sr.reveal('.about-image', {
+    duration: 2000,
+    delay: 400,
+    origin: 'left',
+    viewFactor: 0.2
+  });
+
+  sr.reveal('.about-content', {
+    duration: 2000,
+    delay: 400,
+    origin: 'right',
+    viewFactor: 0.2
+  });
+  
+  //Experience section
+  sr.reveal('.t-right', {
+    duration: 2000,
+    delay: 600,
+    origin: 'right',
+    viewFactor: 0.7
+  });
+
+  sr.reveal('.t-left', {
+    duration: 2000,
+    delay: 600,
+    origin: 'left',
+    viewFactor: 1
+  });
+  
+  //Skills section
+  sr.reveal('.skill-1', {
+    duration: 2000,
+    delay: 400,
+    origin: 'bottom',
+    viewFactor: 0.5
+  });
+
+  sr.reveal('.skill-2', {
+    duration: 2000,
+    delay: 400,
+    origin: 'bottom',
+    viewFactor: 0.5
+  });
+
+  sr.reveal('.skill-3', {
+    duration: 2000,
+    delay: 400,
+    origin: 'bottom',
+    viewFactor: 0.5
+  });
+
+  sr.reveal('.skill-4', {
+    duration: 2000,
+    delay: 400,
+    origin: 'bottom',
+    viewFactor: 0.5
+  });
+
+  //Project section
+  sr.reveal('.project-js', {
+    duration: 2000,
+    delay: 400,
+    origin: 'bottom',
+    viewFactor: 0.5
+  });
+
+//Contact social media section
+  sr.reveal('.social-1', {
+    duration: 2000,
+    delay: 200,
+    origin: 'top',
+    viewFactor: 0.2
+  });
+
+  sr.reveal('.social-2', {
+    duration: 2000,
+    delay: 200,
+    origin: 'top',
+    viewFactor: 0.2
+  });
+
+  sr.reveal('.social-3', {
+    duration: 2000,
+    delay: 200,
+    origin: 'top',
+    viewFactor: 0.2
   });
